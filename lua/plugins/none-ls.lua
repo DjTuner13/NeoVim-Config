d2c433c00f22cd3314b2aa9ba53a21b03041f509
David Ranoia <105903983+DjTuner13@users.noreply.github.com>
2024-04-16 01:19:38 -0400

    format on save added
    added format on save to none-ls/null-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,21 +1,37 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				--null_ls.builtins.formatting.standardts,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.uncrustify,
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.isort,
-        null_ls.builtins.diagnostics.cppcheck,
-        null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.diagnostics.cpplint,
-			},
-		})
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+        -- Set up null-ls
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua,
+                --null_ls.builtins.formatting.standardts,
+                null_ls.builtins.formatting.prettier,
+                null_ls.builtins.formatting.uncrustify,
+                null_ls.builtins.formatting.black,
+                null_ls.builtins.formatting.isort,
+                null_ls.builtins.diagnostics.cppcheck,
+                --null_ls.builtins.diagnostics.eslint_d,
+                --null_ls.builtins.diagnostics.cpplint,
+            },
+            -- Enable auto-format on save
+            on_attach = function(client, bufnr)
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = vim.api.nvim_create_augroup("FormatOnSave", { clear = true }),
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format({ bufnr = bufnr })
+                        end,
+                    })
+                end
+            end,
+        })
+
+        -- Manual format mapping
+        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    end,
 }
+
